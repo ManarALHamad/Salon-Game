3d3c078bf6ebd9ec7735a158bb0eaa181499408b
ManarALHamad <manaralhamad2004@gmail.com>
2026-06-28 16:13:39 +0300

    modify the code and the face with hair is ready

--- app.js
+++ app.js
@@ -5,17 +5,25 @@
 
 /*------------------------ Cached Element References ------------------------*/
 
-const hair = document.querySelector('.hair')
-console.log(hair)
+// const hair = document.querySelector('.hair')
+// console.log(hair)
+const dryer = document.querySelector('.dryer')
+console.log(dryer)
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-hair.addEventListener ('click', function(event){
+// hair.addEventListener ('click', function(event){
 
-    const clickedHair = event.target;
-    console.log('hair clicked')
+//     const clickedHair = event.target;
+//     console.log('hair clicked')
+// })
+
+dryer.addEventListener('move', function(event){
+
+    const dragDryer = event.target;
+    console.log('dryer is drag')
 })
 
 /*-------------------------------- Functions --------------------------------*/
