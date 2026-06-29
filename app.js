448d110cae4dda3b4adcc111758157e7fc20506c
ManarALHamad <manaralhamad2004@gmail.com>
2026-06-29 16:53:17 +0300

    Timer is added when click to start is clicked

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
 
 /*-------------------------------- Constants --------------------------------*/
 // let isPlaying = false;
-    let stopTimer;
+let stopTimer;
+let isDragging = false;
+let offsetX = 0;  //store where the dryer is clicked 
+let offsetY = 0;
+let runTimer;
 
-// let timer = 20;
 // let stop;
 // let isClicked = false;
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
-
+const dryer = document.getElementById("dryer");
 const sound = new Audio ('sounds/hair-dryer-sound.mp3') //adding the blow-dryer sound
-// const startBtn = document.querySelector('#startBtn')
-// const timer = document.querySelector('#timer')
+const startBtn = document.querySelector('#startBtn')
+const timer = document.querySelector('#timer')
 
 /*----------------------------- Event Listeners -----------------------------*/
+//start button with timer 
+startBtn.addEventListener("click", function () {
+
+    // Reset timer
+    time = 10;
+    timer.textContent = "Time: " + time;
+
+    // Prevent multiple intervals
+    clearInterval(runTimer);
+
+    // Start counting down
+    runTimer = setInterval(function () {
+
+        time--;
+        timer.textContent = "Time: " + time;
+
+        console.log("Time left:", time);
+
+        if (time <= 0) {
+
+            clearInterval(runTimer);
+            timer.textContent = "Time's Up!";
+
+            console.log("Game Over");
+
+        }
+
+    }, 1000);
+
+});
+
 
 //drag the hair dryer and point it to the hair
-
-const dryer = document.getElementById("dryer");
-
-
-let isDragging = false;
-let offsetX = 0;  //store where the dryer is clicked 
-let offsetY = 0;
 
 //dragging starts 
 dryer.addEventListener("mousedown", function (event) {
@@ -65,7 +92,11 @@ document.addEventListener("mousemove", function (event) {
 
     dryer.style.left = (event.clientX - offsetX) + "px";
     dryer.style.top = (event.clientY - offsetY) + "px";
+
+    // we will point to the hair using right, left, top, bottom
     // direction of the hair 
+    // dropxone is the hair sections
+
     const hair =document.getElementById("dropZone")
     const dryerRect = dryer.getBoundingClientRect();
     const hairRect = hair.getBoundingClientRect();
